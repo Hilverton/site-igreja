761c391c75c613cc28a30f921ac413bd8b692515
Hilverton Cesar <cesaramojesus@hotmail.com>
2021-03-01 11:27:26 -0300

    💄 Criação do componente Footer

---
+++ components/Footer/index.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className='p-4 bg-gradient-to-l to-my-blue from-my-green font-sans w-full'>
+      <div className='w-11/12 mx-auto flex flex-col md:flex-row justify-center md:justify-between items-center'>
+        <img
+          src='/assets/Logo_assembleia.png'
+          className='h-64'
+          alt='logo igreja sede'
+        />
+      </div>
+    </footer>
+  );
+}
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,10 +1,12 @@
 import Navbar from '../Navbar';
+import Footer from '../Footer';
 
 export default function Layout({ children }) {
   return (
     <div className='w-full min-h-screen bg-cool-gray-100 pt-20'>
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 }
