87bd8947cdb6c69dff2e9acc80ed813fd43c2f62
Hilverton Cesar <cesaramojesus@hotmail.com>
2021-03-01 09:58:01 -0300

    Alteração da pasta de assets da raiz para a pasta public e finalização do componente Navbar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,3 +1,41 @@
+import { useState } from 'react';
+
 export default function Navbar() {
-  return <h1>Navbar</h1>;
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const isOpen = openMenu ? 'h-52 mt-3' : 'h-0';
+
+  return (
+    <header className='p-4 bg-gradient-to-l to-my-blue from-my-green font-sans fixed w-full'>
+      <nav className='flex-col md:flex-row w-full md:inline-flex md:items-center md:justify-between text-white'>
+        <img src='/assets/Logo_Ieadal.png' className='h-12' alt='Logo igreja' />
+        <span
+          className='material-icons absolute top-5 right-4 md:hidden text-3xl'
+          onClick={() => setOpenMenu(!openMenu)}
+        >
+          menu
+        </span>
+        <div className={`${isOpen} transition-all duration-300 md:flex h-auto`}>
+          <ul
+            className={`${
+              openMenu ? 'flex-col space-y-4' : 'hidden'
+            } w-full md:w-min md:flex md:space-x-4`}
+          >
+            <li className='p-2 z-0 md:px-5 rounded-lg uppercase text-white hover:text-black hover:bg-white duration-300 cursor-pointer'>
+              Início
+            </li>
+            <li className='p-2 z-0 md:px-5 rounded-lg uppercase text-white hover:text-black hover:bg-white duration-300 cursor-pointer'>
+              Departamentos
+            </li>
+            <li className='p-2 z-0 md:px-5 rounded-lg uppercase text-white hover:text-black hover:bg-white duration-300 cursor-pointer'>
+              Ebd
+            </li>
+            <li className='p-2 z-0 md:px-5 rounded-lg uppercase text-white hover:text-black hover:bg-white duration-300 cursor-pointer'>
+              Notícias
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { Navbar } from '../components';
+
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-      <h1 className='text-3xl text-green-500'>Site Igreja</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+        <h1 className='text-3xl text-green-500'>Site Igreja</h1>
+      </div>
+    </>
   );
 }
