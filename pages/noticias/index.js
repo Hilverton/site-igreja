14076cc4ba8db8297994b1008d284cf48952318f
Hilverton Cesar <cesaramojesus@hotmail.com>
2021-03-02 13:19:15 -0300

    ✨Criação da página Notícias

---
+++ pages/noticias/index.js
@@ -0,0 +1,11 @@
+import { Layout } from '../../components';
+
+export default function Noticias() {
+  return (
+    <Layout>
+      <section className='w-11/12 mx-auto py-6 flex justify-center items-center h-screen'>
+        <h1 className='text-3xl'>Página de notícias em construção🚧</h1>
+      </section>
+    </Layout>
+  );
+}
