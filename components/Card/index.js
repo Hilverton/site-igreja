b19b02a32bedef4941993d8aac299d202210fad7
Hilverton Cesar <cesaramojesus@hotmail.com>
2021-03-03 10:32:08 -0300

    💄 Ajuste do componente Card para a página Notícias

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,12 +1,22 @@
-export default function Card({ title }) {
+export default function Card({ title, big }) {
+  const isBig = big ? 'md:flex md:p-6 ' : 'h-full';
+  const imgRounded = big && 'md:rounded-lg';
+  const body = big ? 'p-6 md:py-2 md:pl-6 md:pr-3' : 'p-6';
+
   return (
-    <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-white'>
-      <img
-        className='lg:h-48 md:h-36 w-full object-cover object-center'
-        src='https://dummyimage.com/720x400'
-        alt='blog'
-      />
-      <div className='p-6'>
+    <div
+      className={`${isBig} border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-white`}
+    >
+      <figure
+        className={`lg:h-48 md:h-36 w-full object-cover object-center ${imgRounded} overflow-hidden`}
+      >
+        <img
+          className='w-full h-full'
+          src='https://dummyimage.com/720x400'
+          alt='blog'
+        />
+      </figure>
+      <div className={body}>
         <h1 className='title-font text-lg font-medium text-gray-900 mb-3'>
           {title}
         </h1>
