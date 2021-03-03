36b568537abf2515b0e35122d8f2e0b25b9b878f
Hilverton Cesar <cesaramojesus@hotmail.com>
2021-03-03 10:47:50 -0300

    ✨ Página Notícias quase finalizada

--- pages/noticias/index.js
+++ pages/noticias/index.js
@@ -1,10 +1,17 @@
-import { Layout } from '../../components';
+import { Layout, Card } from '../../components';
 
 export default function Noticias() {
   return (
     <Layout>
-      <section className='w-11/12 mx-auto py-6 flex justify-center items-center h-screen'>
-        <h1 className='text-3xl'>Página de notícias em construção🚧</h1>
+      <section className='w-11/12 mx-auto py-6'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+          <div className='col-span-1 md:col-span-2 space-y-6'>
+            <Card title='Culto de Doutrina' big />
+            <Card title='Culto Evangelístico' big />
+            <Card title='Culto de Doutrina' big />
+            <Card title='Culto de Rua' big />
+          </div>
+        </div>
       </section>
     </Layout>
   );
